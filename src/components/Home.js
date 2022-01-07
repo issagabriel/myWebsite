45dac1ae1dfca7ef352b240b2142d5f4a874b358
Gabriel Issa <issag2016@gmail.com>
2022-01-07 10:24:14 -0500

    second

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <h1>Hello, my name is Gabriel Issa</h1>
+      <h3>I am a full-stack web developer and I specialize in JavaScript.</h3>
+    </div>
+  );
 };
 
 export default Home;
