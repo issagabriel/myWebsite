5932e83ee9b3209602e2ebb98fdff847ecbaf992
Gabriel Issa <issag2016@gmail.com>
2022-01-10 13:26:36 -0500

    third

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <h1>Hello, my name is Gabriel Issa</h1>
       <h3>I am a full-stack web developer and I specialize in JavaScript.</h3>
     </div>
