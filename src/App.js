06807ab65d4d16f99118ee2441fc1657a0491e0c
Gabriel Issa <issag2016@gmail.com>
2022-01-11 10:58:03 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Navbar />
-      <Landing />
+      <Switch>
+        <Route exact path="/" component={(props) => <Landing {...props} />} />
+      </Switch>
     </div>
   );
 }
