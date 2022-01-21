151f6e501a56d3732cdab283646ee455c16a1536
Gabriel Issa <issag2016@gmail.com>
2022-01-21 12:49:45 -0500

    1/21

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,9 @@ const Projects = () => {
   return (
     <div className="projects">
       <h1>Projects</h1>
+      <h3>Mega Birds</h3>
+      <h3>Sports Blog</h3>
+      <h3>Sports Hub</h3>
     </div>
   );
 };
