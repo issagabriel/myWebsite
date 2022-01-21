6dd2ec9a14c2b69fa1d4afbf74b60409574c930f
Gabriel Issa <issag2016@gmail.com>
2022-01-21 12:59:42 -0500

    1/21

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Landing />
       <About />
       <Projects />
+      <Footer />
     </div>
   );
 }
